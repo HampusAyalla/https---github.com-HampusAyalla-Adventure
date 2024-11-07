ffe58f8c3fcb124f0ec5f25313f3617689742812
HampusAyalla <hampus.melinosterlindh@skola.taby.se>
2024-11-07 10:44:50 +0100

    First commit

--- äventyr.py
+++ äventyr.py
@@ -1,1 +1,9 @@
-#test
+Level = 0
+Health = 10
+Strenght = 50
+
+import random
+print("Du är fast i en djungel. Du ser tre öppningar framför dig en till vänster en till höger och en rakt fram.")
+direction = input("Vilket håll vill du gå åt? --> ").lower()
+
+faith_list = ["monster","monster","monster","monster","monster","monster","monster","monster","monster","monster","monster","monster","monster","monster","monster","monster","chest","chest","chest","chest","chest","chest","chest","chest","chest","chest","chest","chest","trap","trap","trap","trap","trap","trap","trap","trap",]
