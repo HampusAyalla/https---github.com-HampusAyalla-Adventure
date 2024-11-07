95fa8fa43aee7cab4ebeafd48f0b8aed4c122dc4
HampusAyalla <hampus.melinosterlindh@skola.taby.se>
2024-11-07 11:29:34 +0100

    Första lektion

--- äventyr.py
+++ äventyr.py
@@ -6,4 +6,15 @@ import random
 print("Du är fast i en djungel. Du ser tre öppningar framför dig en till vänster en till höger och en rakt fram.")
 direction = input("Vilket håll vill du gå åt? --> ").lower()
 
-faith_list = ["monster","monster","monster","monster","monster","monster","monster","monster","monster","monster","monster","monster","monster","monster","monster","monster","chest","chest","chest","chest","chest","chest","chest","chest","chest","chest","chest","chest","trap","trap","trap","trap","trap","trap","trap","trap",]
+faith_list = ["panther","panther","panther","panther","panther","aligator","aligator","aligator","aligator","aligator","tribe member","tribe member","tribe member","tribe member","tribe member","stone golem","chest","chest","chest","chest","chest","chest","chest","chest","chest","chest","chest","chest","trap","trap","trap","trap","trap","trap","trap","trap","shop"]
+left = faith_list[random.randint(0,36)]
+straight = faith_list[random.randint(0,36)]
+right = faith_list[random.randint(0,36)]
+if direction == "vänster":
+    faith = left
+elif direction == "rakt fram":
+    faith = straight
+elif direction == "höger":
+    faith = right
+else:
+    direction = input("Du måste svara vänster, höger eller rakt fram. --> ").lower
